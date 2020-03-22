f2fde1ef95658b1be836adbaa40614b8da34b96c
Arkadiusz Suchenia <arek@arnonedev.pl>
2020-03-22 20:54:18 +0100

    RSN-7 - Added AddNewName component

---
+++ ui/src/app/components/add-new-name/add-new-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-name',
+  templateUrl: './add-new-name.component.html',
+  styleUrls: ['./add-new-name.component.scss']
+})
+export class AddNewNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
