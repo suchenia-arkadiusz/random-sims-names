381510721b2ffacd9d9e033f485b4a2653079520
Arkadiusz Suchenia <arek@arnonedev.pl>
2020-03-22 11:10:12 +0100

    RSN-6 - Created empty angular project for UI app
    Moved server code to 'server' directory

---
+++ server/src/test/java/pl/arusoftware/randomsimsname/RandomSimsNameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.arusoftware.randomsimsname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomSimsNameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
