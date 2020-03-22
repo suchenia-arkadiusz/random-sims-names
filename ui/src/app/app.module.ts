1886f826c6c0041d4ef18f0d1916f3779c25e08b
Arkadiusz Suchenia <arek@arnonedev.pl>
2020-03-22 11:57:44 +0100

    RSN-7 - Added base layout components

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PopupComponent } from './components/popup/popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
