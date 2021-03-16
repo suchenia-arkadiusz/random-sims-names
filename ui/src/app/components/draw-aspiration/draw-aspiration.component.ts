14280ef5a7e9fe141e09ca0fc1683661aa0a4f3d
Arkadiusz Suchenia <suchenia.arkadiusz@aru-software.pl>
2021-03-16 19:28:22 +0100

    (feat/GH-13) Add 'Draw aspiration' empty component

---
+++ ui/src/app/components/draw-aspiration/draw-aspiration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-draw-aspiration',
+  templateUrl: './draw-aspiration.component.html',
+  styleUrls: ['./draw-aspiration.component.scss']
+})
+export class DrawAspirationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
